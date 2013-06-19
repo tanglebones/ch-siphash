bd3bd28db6eb715ea8be8bae73fc9125053b9ccf
Cliff Hammerschmidt <tanglebones@gmail.com>
2013-06-19 11:22:53 -0700

    sign assemblies.

--- CH.SipHash/Properties/AssemblyInfo.cs
+++ CH.SipHash/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("CH.SipHash.Test")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: InternalsVisibleTo("CH.SipHash.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b58e9064ba1a919ab967e99eb38276cf108698f17b2905ff3d7f6e60857075c867c0cabef4d52e8a47fdf945c5a9c873bdc6edca0ee6e6549642ee794b58bb662d4ee3fd18039cd39aa1be1a58d148048af834d986e1e25ceb31c77491b83b94d115495cc6620c955485d780e3d85e6e2b8b132b9bea0a0753868f03f82895e9")]
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ ch-siphash
 ==========
 
 siphash in c# using unsafe for performance, based on the reference C code.
+
+The SipHash_2_4_UlongCast_ForcedInline version is faster on my machine.
